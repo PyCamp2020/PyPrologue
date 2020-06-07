64a9201600362d25c08dfe90b020a7ea8033dc07
NikoZHAI <huanyu.zhai@etu.unice.fr>
2020-06-07 09:08:42 +0800

    update exo1.pu comments

--- exercise1/exo1.py
+++ exercise1/exo1.py
@@ -1,40 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-Python 练习一
+@breif Python 练习一
+@author si.chen
 
-计算年均雨量
+目标: 计算某一流域的年均雨量
 
-@author: si
+知识点:
+	1. 如何导入一个库				:	import 	语句
+	2. 如何定义方法(函数)			:	def 	语句
+	3. 如何定义变量				:	<变量名> = <值>
+	4. 使用 os 模块中路径相关操作	:	getcwd(), listdir(), chdir() 方法
+	5. 如何使用 pandas 库读取 csv 	:	read_csv() 方法
+	6. 去掉 DataFrame 中的缺失行	:	dropna() 方法
 """
+
 
 # 引入相关库
 import os
 import pandas
 
 
-def getCwd():
+"""
+============
+定义方法(函数)
+============
+
+Python 中使用 `def` 关键字来定义函数, 格式如下:
+
+def <函数名>([<参数1>, <参数2>...<参数n>]):
+	<函数定义>
+	return <返回值>
+
+例如我们在这里定义一个 `get_cwd()` 方法, 用于获取当前工作路径,
+作为 `os.getcwd()` 的短手 (short-hand)
+
+"""
+def get_cwd():
     return os.getcwd()
 
 
-"""
-获取当前路径
+# Python3 中打印方法为 print()
+print("当前工作路径 = ")
+print(get_cwd())
 
-@note Python 中变量无需声明， 可以直接赋值
-"""
-print(getCwd())
+# 列出初始工作路径下的内容
+print("当前路径下的内容 = ")
+os.listdir()
 
 # 改变工作路径到 data
 os.chdir(path='F:\PyPrologue\data')
+print("我们改变后的工作路径 = ")
+print(get_cwd())
 
-# 列出当前路径下的内容
+# 列出路径下的内容
+print("当前路径下的内容 = ")
 os.listdir()
 
 
 # 小知识： 编程语言都是显式的 (explicit)
 """
-========
+=======
 读取数据
-========
+=======
 
 1. 分隔符:
     我们在读取一个 CSV 文件时， 首先要注意它的分隔符是否为 `pandas.read_csv()` 默认的 ','， 若不是，
@@ -46,9 +73,14 @@ os.listdir()
 4. 指定想导入的列:
     使用 `usecols` 这一参数
 """
-df_raw = pandas.read_csv('BV_38.txt',
-                         comment='#', 
-                         usecols=['Date','Q','Ptot'],
-                         header=0, sep=';', skipinitialspace=True)
+df_raw = pandas.read_csv(
+	'BV_38.txt',
+    comment='#',
+    sep=';', 
+    header=0,
+    usecols=['Date','Q','Ptot'],
+    skipinitialspace=True
+)
 
-df_neat = df_raw.dropna()
+# 去掉 DataFrame 中的缺失行, `inplace` 参数代表是否直接在原变量中操作
+df_raw.dropna(inplace=True)
